375087cbf576825f9e130ed797d0c3e6c78d5c57
Роман <romangribanov@rambler.ru>
2025-01-29 12:32:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# "1st program".
+print(9 ** 0.5 * 5)
+
+# "2nd program".
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# "3rd program".
+a = (2 * 2 + 2)
+print(a)
+b = (2 * (2 + 2))
+print(b)
+c = a == b
+print(c)
+
+# "4th program".
+print(int(float('123.456') * 10) % 10)
